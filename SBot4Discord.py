69cba033cbe69754e48cc7ccf1d3a90c64199af0
dim5x <dim5x@yahoo.com>
2021-03-04 04:26:24 +0300

    Fourth.

--- SBot4Discord.py
+++ SBot4Discord.py
@@ -17,7 +17,7 @@ async def clr(ctx, amount: int):
     """Удаляет сообщения. !clean N -  удалит N последних сообщений. """
 
     await ctx.channel.purge(limit=amount + 1)
-    await ctx.send(f"{amount} message(s) got deleted")
+    await ctx.send(f"{amount} сообщений было удалено.")
     time.sleep(5)
     await ctx.channel.purge(limit=1)
 
